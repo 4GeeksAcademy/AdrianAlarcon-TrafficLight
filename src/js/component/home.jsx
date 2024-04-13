cb1bfe736ad59b44f5135e7640df8b16532a334c
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-13 09:45:46 +0000

    funcionalidad princiapal con state hehca

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,15 +6,39 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	
+	const [selectedLight, setSelectedLight] = useState(null);
+
+	const handleLightClick = (light) => {
+		if (selectedLight === light) {
+			setSelectedLight(null);
+		}else{
+			setSelectedLight(light);
+		}
+	}
+
+	const handleOutsideClick = () => {
+		setSelectedLight(null);
+	}
 
 	return (
-		<div className="midContainer">
+		<div className="midContainer" onClick={() => handleOutsideClick()}>
 			<div className="stick"></div>
 			<div className="trafficLight">
-				<div className="red light"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
+				<div className={`red light ${selectedLight === "red" ? "selected" : ""}`}
+					onClick={(e) => {
+						e.stopPropagation();
+						handleLightClick("red");
+					}}></div>
+				<div className={`yellow light ${selectedLight === "yellow" ? "selected" : ""}`}
+					onClick={(e) => {
+						e.stopPropagation();
+						handleLightClick("yellow");
+					}}></div>
+				<div className={`green light ${selectedLight === "green" ? "selected" : ""}`}
+					onClick={(e) => {
+						e.stopPropagation();
+						handleLightClick("green");
+					}}></div>
 			</div>
 		</div>
 	);
